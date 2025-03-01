aa5151ab5ce16d9ed5f6ebf9441fb42e9a9cfe4b
USER <20f91a0490pec@gmail.com>
2025-03-01 12:55:03 +0530

    i am changing the file

---
+++ src/VariableDemo2.java
@@ -0,0 +1,19 @@
+
+public class VariableDemo2 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("10");
+		System.out.println(10);
+		System.out.println("10+5");
+		System.out.println(10+5);
+		int a=10;
+		System.out.println(a);
+		System.out.println("a");
+		System.out.println(a+5);
+		System.out.println("a+5");
+	}
+
+
+}
